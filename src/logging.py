00c8292dae7774d903d79a21f30af1e2ecf21258
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2023-04-07 04:10:20 -0300

    Ignore /health accesses for logging purposes

--- src/logging.py
+++ src/logging.py
@@ -15,3 +15,18 @@ def warn(msg: str) -> None:
 
 def error(msg: str) -> None:
     logging.getLogger("uvicorn").error(msg)
+
+
+# disable /health endpoint logging
+class EndpointFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        if record.args is None or len(record.args) >= 3:
+            return True
+
+        if not isinstance(record.args, tuple):
+            return True
+
+        return record.args[2] != "/health"
+
+
+logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
