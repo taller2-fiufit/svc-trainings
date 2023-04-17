ac6b0453218af843bfcfda8bdff9df449544485e
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-04-16 23:54:39 -0300

    Disable cookies on CORS

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ app = FastAPI(lifespan=lifespan, title="Kinetix", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
