1aa29625a8f57b390e12f8fd728bbf93da7c0d6d
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-04-18 23:56:17 -0300

    Use AsyncClient instead of TestClient

--- src/api/trainings_test.py
+++ src/api/trainings_test.py
@@ -1,33 +1,26 @@
 from typing import Awaitable, Callable
-from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
 from src.auth import get_user, ignore_auth
 from src.db.migration import downgrade_db
-# from src.db.model.training import TrainingType
-# from src.api.model.training import CreateTraining
+from src.db.model.training import TrainingType
+from src.api.model.training import CreateTraining
 from src.main import app, lifespan
 
 
-def with_client(
-    test: Callable[[TestClient], Awaitable[None]]
-) -> Callable[[], Awaitable[None]]:
-    async def decorated_test() -> None:
-        # reset database
-        await downgrade_db()
+@pytest.fixture
+async def client() -> AsyncGenerator[AsyncClient, None]:
+    # reset database
+    await downgrade_db()
 
-        # https://fastapi.tiangolo.com/advanced/testing-dependencies/
-        app.dependency_overrides[get_user] = ignore_auth
+    # https://fastapi.tiangolo.com/advanced/testing-dependencies/
+    app.dependency_overrides[get_user] = ignore_auth
 
-        # lifespan is not used automatically
-        async with lifespan(app) as _:
-            client = TestClient(app)
-            await test(client)
-
-    return decorated_test
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
 
 
-@with_client
-async def test_trainings_get_empty(client: TestClient) -> None:
+async def test_trainings_get_empty(client: AsyncClient) -> None:
     response = client.get("/trainings/")
 
     assert response.status_code == 200
@@ -37,158 +30,152 @@ async def test_trainings_get_empty(client: TestClient) -> None:
     assert json == []
 
 
-# @with_client
-# async def test_trainings_get_nonexistent(client: TestClient) -> None:
-#     response = client.get("/trainings/1")
+async def test_trainings_get_nonexistent(client: AsyncClient) -> None:
+    response = client.get("/trainings/1")
 
-#     assert response.status_code == 404
-
-
-# @with_client
-# async def test_trainings_post(client: TestClient) -> None:
-#     body = CreateTraining(
-#         title="title",
-#         description="description",
-#         type=TrainingType.WALK,
-#         difficulty=1,
-#     )
-
-#     response = client.post("/trainings/", json=body.dict())
-
-#     assert response.status_code == 201
-
-#     result = CreateTraining(**response.json())
-
-#     assert result == body
+    assert response.status_code == 404
 
 
-# @with_client
-# async def test_trainings_post_no_body(client: TestClient) -> None:
-#     response = client.post("/trainings/")
-#     assert response.status_code == 422
+async def test_trainings_post(client: AsyncClient) -> None:
+    body = CreateTraining(
+        title="title",
+        description="description",
+        type=TrainingType.WALK,
+        difficulty=1,
+    )
+
+    response = client.post("/trainings/", json=body.dict())
+
+    assert response.status_code == 201
+
+    result = CreateTraining(**response.json())
+
+    assert result == body
 
 
-# @with_client
-# async def test_trainings_post_get(client: TestClient) -> None:
-#     response = client.get("/trainings/")
-#     assert response.status_code == 200
-#     assert response.json() == []
-
-#     body = CreateTraining(
-#         title="title",
-#         description="description",
-#         type=TrainingType.WALK,
-#         difficulty=1,
-#     )
-#     response = client.post("/trainings/", json=body.dict())
-#     assert response.status_code == 201
-
-#     response = client.get("/trainings/")
-#     assert response.status_code == 200
-#     assert len(response.json()) == 1
-
-#     got = response.json()[0]
-
-#     assert got["title"] == body.title
-#     assert got["description"] == body.description
-#     assert got["type"] == body.type
-#     assert got["difficulty"] == body.difficulty
-
-#     response = client.get(f"/trainings/{got['id']}")
-#     assert response.status_code == 200
-#     assert got == response.json()
+async def test_trainings_post_no_body(client: AsyncClient) -> None:
+    response = client.post("/trainings/")
+    assert response.status_code == 422
 
 
-# @with_client
-# async def test_trainings_patch(client: TestClient) -> None:
-#     body = CreateTraining(
-#         title="title",
-#         description="description",
-#         type=TrainingType.RUNNING,
-#         difficulty=1,
-#     )
-#     response = client.post("/trainings/", json=body.dict())
-#     assert response.status_code == 201
+async def test_trainings_post_get(client: AsyncClient) -> None:
+    response = client.get("/trainings/")
+    assert response.status_code == 200
+    assert response.json() == []
 
-#     body.description = "new description"
-#     body.type = TrainingType.WALK
+    body = CreateTraining(
+        title="title",
+        description="description",
+        type=TrainingType.WALK,
+        difficulty=1,
+    )
+    response = client.post("/trainings/", json=body.dict())
+    assert response.status_code == 201
 
-#     response = client.patch(
-#         f"/trainings/{response.json()['id']}", json=body.dict()
-#     )
-#     assert response.status_code == 200
+    response = client.get("/trainings/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
 
-#     got = response.json()
+    got = response.json()[0]
 
-#     assert got["title"] == body.title
-#     assert got["description"] == body.description
-#     assert got["type"] == body.type
-#     assert got["difficulty"] == body.difficulty
+    assert got["title"] == body.title
+    assert got["description"] == body.description
+    assert got["type"] == body.type
+    assert got["difficulty"] == body.difficulty
 
-#     response = client.get(f"/trainings/{got['id']}")
-#     assert response.status_code == 200
-#     assert got == response.json()
+    response = client.get(f"/trainings/{got['id']}")
+    assert response.status_code == 200
+    assert got == response.json()
 
 
-# @with_client
-# async def test_trainings_invalid_body(client: TestClient) -> None:
-#     # valid body
-#     body = CreateTraining(
-#         title="title",
-#         description="description",
-#         type=TrainingType.WALK,
-#         difficulty=1,
-#     )
+async def test_trainings_patch(client: AsyncClient) -> None:
+    body = CreateTraining(
+        title="title",
+        description="description",
+        type=TrainingType.RUNNING,
+        difficulty=1,
+    )
+    response = client.post("/trainings/", json=body.dict())
+    assert response.status_code == 201
 
-#     response = client.post("/trainings/", json=body.dict())
-#     assert response.status_code == 201
+    body.description = "new description"
+    body.type = TrainingType.WALK
 
-#     body.title = "other title"
+    response = client.patch(
+        f"/trainings/{response.json()['id']}", json=body.dict()
+    )
+    assert response.status_code == 200
 
-#     # too long title
-#     json = body.dict()
-#     json["title"] = "a" * 31
+    got = response.json()
 
-#     response_post = client.post("/trainings/", json=json)
-#     response_patch = client.patch("/trainings/1", json=json)
-#     assert response_post.status_code == response_patch.status_code == 422
+    assert got["title"] == body.title
+    assert got["description"] == body.description
+    assert got["type"] == body.type
+    assert got["difficulty"] == body.difficulty
 
-#     # short title
-#     json = body.dict()
-#     json["title"] = ""
+    response = client.get(f"/trainings/{got['id']}")
+    assert response.status_code == 200
+    assert got == response.json()
 
-#     response_post = client.post("/trainings/", json=json)
-#     response_patch = client.patch("/trainings/1", json=json)
-#     assert response_post.status_code == response_patch.status_code == 422
 
-#     # too long description
-#     json = body.dict()
-#     json["description"] = "a" * 301
+async def test_trainings_invalid_body(client: AsyncClient) -> None:
+    # valid body
+    body = CreateTraining(
+        title="title",
+        description="description",
+        type=TrainingType.WALK,
+        difficulty=1,
+    )
 
-#     response_post = client.post("/trainings/", json=json)
-#     response_patch = client.patch("/trainings/1", json=json)
-#     assert response_post.status_code == response_patch.status_code == 422
+    response = client.post("/trainings/", json=body.dict())
+    assert response.status_code == 201
 
-#     # nonexistent type
-#     json = body.dict()
-#     json["type"] = "nonexistent type"
+    body.title = "other title"
 
-#     response_post = client.post("/trainings/", json=json)
-#     response_patch = client.patch("/trainings/1", json=json)
-#     assert response_post.status_code == response_patch.status_code == 422
+    # too long title
+    json = body.dict()
+    json["title"] = "a" * 31
 
-#     # too high difficulty
-#     json = body.dict()
-#     json["difficulty"] = "11"
+    response_post = client.post("/trainings/", json=json)
+    response_patch = client.patch("/trainings/1", json=json)
+    assert response_post.status_code == response_patch.status_code == 422
 
-#     response_post = client.post("/trainings/", json=json)
-#     response_patch = client.patch("/trainings/1", json=json)
-#     assert response_post.status_code == response_patch.status_code == 422
+    # short title
+    json = body.dict()
+    json["title"] = ""
 
-#     # negative difficulty
-#     json = body.dict()
-#     json["difficulty"] = "-1"
+    response_post = client.post("/trainings/", json=json)
+    response_patch = client.patch("/trainings/1", json=json)
+    assert response_post.status_code == response_patch.status_code == 422
 
-#     response_post = client.post("/trainings/", json=json)
-#     response_patch = client.patch("/trainings/1", json=json)
-#     assert response_post.status_code == response_patch.status_code == 422
+    # too long description
+    json = body.dict()
+    json["description"] = "a" * 301
+
+    response_post = client.post("/trainings/", json=json)
+    response_patch = client.patch("/trainings/1", json=json)
+    assert response_post.status_code == response_patch.status_code == 422
+
+    # nonexistent type
+    json = body.dict()
+    json["type"] = "nonexistent type"
+
+    response_post = client.post("/trainings/", json=json)
+    response_patch = client.patch("/trainings/1", json=json)
+    assert response_post.status_code == response_patch.status_code == 422
+
+    # too high difficulty
+    json = body.dict()
+    json["difficulty"] = "11"
+
+    response_post = client.post("/trainings/", json=json)
+    response_patch = client.patch("/trainings/1", json=json)
+    assert response_post.status_code == response_patch.status_code == 422
+
+    # negative difficulty
+    json = body.dict()
+    json["difficulty"] = "-1"
+
+    response_post = client.post("/trainings/", json=json)
+    response_patch = client.patch("/trainings/1", json=json)
+    assert response_post.status_code == response_patch.status_code == 422
