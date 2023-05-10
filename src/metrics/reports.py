7b88767e1ef20e04d31adf55f25d6be1a601b907
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2023-05-09 22:00:30 -0300

    Add `attrs` attribute to report (#11)
    * Add attrs attribute to report
    
    * Add .flake8 and fix errors

--- src/metrics/reports.py
+++ src/metrics/reports.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Callable
 
 from pydantic import BaseModel
+from src.api.model.training import Training
 
 from src.metrics.queue import QUEUE
 
@@ -15,21 +16,22 @@ class TrainingCreationReport(BaseModel):
     service: str = SERVICE_NAME
     command: str = CMD_CREATED
     timestamp: datetime
+    attrs: Training
 
     @classmethod
-    def from_now(cls) -> "TrainingCreationReport":
-        return TrainingCreationReport(timestamp=datetime.now())
+    def from_now(cls, attrs: Training) -> "TrainingCreationReport":
+        return TrainingCreationReport(timestamp=datetime.now(), attrs=attrs)
 
 
-def report_training_creation(id: int) -> None:
-    body = TrainingCreationReport.from_now().json()
+def report_training_creation(training: Training) -> None:
+    body = TrainingCreationReport.from_now(attrs=training).json()
 
     if QUEUE.url.endswith(".fifo"):
         # Send message with SQS FIFO-only parameters
         _ = QUEUE.send_message(
             MessageBody=body,
             # MessageDeduplicationId and MessageGroupId only for FIFO queues
-            MessageDeduplicationId=f"{SERVICE_NAME}-{CMD_CREATED}-{id}",
+            MessageDeduplicationId=f"{SERVICE_NAME}-{CMD_CREATED}-{training.id}",
             MessageGroupId=MESSAGE_GROUP_ID,
         )
     else:
@@ -38,5 +40,5 @@ def report_training_creation(id: int) -> None:
         )
 
 
-def get_reporter() -> Callable[[int], None]:
+def get_reporter() -> Callable[[Training], None]:
     return report_training_creation
